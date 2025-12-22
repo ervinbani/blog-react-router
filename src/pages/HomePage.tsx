a20a29bf85da4604b27a9bfdd2af0102b64ce4cd
ervinbani <bani.ervin@gmail.com>
2025-12-22 17:26:50 +0100

    Add HomePage component with welcome message and description

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Blog!</h1>
+      <p>This is the Home Page. Explore posts, products, and more.</p>
+    </div>
+  );
+};
+
+export default HomePage;
