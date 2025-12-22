48f7eb0eb2babeeb217d85791099164fd0d208e0
ervinbani <bani.ervin@gmail.com>
2025-12-22 17:29:25 +0100

    Fix formatting and remove unnecessary blank lines in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
+import { Routes, Route, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ProductsPage from "./pages/ProductsPage";
+import "./App.css";
 
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ProductsPage from './pages/ProductsPage';
-import './App.css';
-
-
-function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/products" element={<ProductsPage />} />
-    </Routes>
+    <div>
+      <nav style={{ marginBottom: '1.5rem' }}>
+        <ul style={{ display: 'flex', gap: '1rem', listStyle: 'none', padding: 0 }}>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/products">Products</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
+    </div>
   );
 }
 
-export default App
+export default App;
