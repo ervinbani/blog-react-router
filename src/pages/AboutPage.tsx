bc0a98269de0aac4278970be2ad4fdcebd8eb8cf
ervinbani <bani.ervin@gmail.com>
2025-12-22 17:27:28 +0100

    Fix formatting in HomePage component and add AboutPage component

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage: React.FC = () => {
+  return (
+    <div>
+      <h1>About This Blog</h1>
+      <p>This is a simple blog application built with React and React Router for learning purposes. Here you can find posts, product info, and more!</p>
+    </div>
+  );
+};
+
+export default AboutPage;
